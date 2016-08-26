85169047c266db9bedd8a52a4a14e458df3b37f9
SantosM1281 <santosm@sm-portfolio.com>
2016-08-26 14:06:24 -0400

    fonts uploaded

---
+++ includes/js/gallery.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+		
+			jQuery(function( $ ) {
+				$('#example-2 a').tosrus({
+					pagination	: {
+						add			: true
+					},
+					caption		: {
+						add			: true
+					},
+					slides		: {
+						scale		: 'false'
+					}
+				});
+			});
+		</script>
